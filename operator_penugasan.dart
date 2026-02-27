a89f1bcef8ad6e9afd2859d431f315193eb785fc
SASCHADDD <saschadanu23@gmail.com>
2026-02-27 16:30:49 +0700

    membuat aritmatika dengan menambahkan data lain untuk di hitung

---
+++ operator_penugasan.dart
@@ -0,0 +1,16 @@
+void main() {
+  int a = 10;
+  double b = 10;
+  // int b = 5;
+  int penjumlahandenganassignment = a += 5;
+  int pengurangandenganassignment = a -= 5;
+  int perkaliandanassignment = a *= 5;
+  double pembagiandenganassignment = b /= 5;
+  int pembagianbilanganbulatdenganassignment = a ~/= 5;
+
+  print("$penjumlahandenganassignment");
+  print("$pengurangandenganassignment");
+  print("$perkaliandanassignment");
+  print("$pembagiandenganassignment");
+  print("$pembagianbilanganbulatdenganassignment");
+}
