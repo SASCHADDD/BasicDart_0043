61636d391f802fba71ab4176d05167d4575874ef
SASCHADDD <saschadanu23@gmail.com>
2026-02-27 15:52:04 +0700

    membuat nilai final

---
+++ variableimmutablefinal.dart
@@ -0,0 +1,8 @@
+void main() {
+var nama = "Sascha Danu";
+  nama = "Aca";
+
+final name = "Danu"; // Tidak bisa diubah setelah deklarasi print(nameFinal);
+//name = "Lior"; // ERROR: Tidak bisa mengubah nilai final
+  print(name);
+}
