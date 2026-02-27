3dc25ff901ca934815cfafb3190306208abdcc7f
SASCHADDD <saschadanu23@gmail.com>
2026-02-27 16:07:20 +0700

    menabhkan 2 operator lagi

--- operator_aritmatika.dart
+++ operator_aritmatika.dart
@@ -5,9 +5,13 @@ void main () {
   int pengurangan = a - b;
   int perkalian = a * b;
   double pembagian = a / b;
-  
+  int pembagianbilanganbulat = a ~/ b;
+  int modulus = a % b;
+
   print("Hasil Penjumlahan: $penjumlahan");
   print("Hasil Pengurangan: $pengurangan");
   print("Hasil Perkalian: $perkalian");
   print("Hasil Pembagian: $pembagian");
+  print("Hasil Pembagian Bilangan Bulat: $pembagianbilanganbulat");
+  print("Hasil Modulus: $modulus");
 }
