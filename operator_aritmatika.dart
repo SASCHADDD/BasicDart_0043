f9044a52baaac39fbc5525033f4ac24aa3836bbe
SASCHADDD <saschadanu23@gmail.com>
2026-02-27 16:02:18 +0700

    membuat operator dalam aritmatika

---
+++ operator_aritmatika.dart
@@ -0,0 +1,13 @@
+void main () {
+  int a = 10;
+  int b = 5;
+  int penjumlahan = a + b;
+  int pengurangan = a - b;
+  int perkalian = a * b;
+  double pembagian = a / b;
+  
+  print("Hasil Penjumlahan: $penjumlahan");
+  print("Hasil Pengurangan: $pengurangan");
+  print("Hasil Perkalian: $perkalian");
+  print("Hasil Pembagian: $pembagian");
+}
